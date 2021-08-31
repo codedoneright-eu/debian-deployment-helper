a0dc90383c01db6f17bfabbc097bdb1bfda4807e
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2021-08-31 23:24:25 +0200

    Update 99_adduser.sh

--- data/99_adduser.sh
+++ data/99_adduser.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/expect -f
 
-#This module is not implemented, thinking if it is even necessary at all 
+#This module is not implemented, thinking if necessary at all 
 
-read -p "Please provide new username " NEW_USERNAME
-read -p "Please provide password " NEW_PASSWORD
+read -p "Please provide the new username " NEW_USERNAME
+read -p "Please provide the password " NEW_PASSWORD
 
 #Add new user
 spawn sudo adduser ${NEW_USERNAME}
