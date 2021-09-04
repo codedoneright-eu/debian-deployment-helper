5ec71acbc59e5196df483033e2b279d6c43f954a
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2021-09-04 10:23:47 +0200

    Update 06_shares.sh

--- data/06_shares.sh
+++ data/06_shares.sh
@@ -2,20 +2,11 @@
 
 if [ $SHARE_INSTALL = y ]; then
 
-	mkdir /home/${SHARE_USER}/${SHARE_FOLDER}
-	chown ${SHARE_USER}:${SHARE_USER} -R /home/${SHARE_USER}/${SHARE_FOLDER}
-	
-	echo "${SHARE_PATH}	/home/${SHARE_USER}/${SHARE_FOLDER}	cifs	uid=1000,guest,rw,users,nofail	0	0" >> /etc/fstab
-	
-	echo ""
-	echo "Network share has been mounted for ${SHARE_USER} in ~/${SHARE_FOLDER}"
-	echo "nofail option ensures no boot-loop if the share is unavailable"
+	mkdir /home/${USERNAME_INSTALL}/${SHARE_FOLDER}
+	chown ${USERNAME_INSTALL}:${USERNAME_INSTALL} -R /home/${USERNAME_INSTALL}/${SHARE_FOLDER}
+
+	echo "${SHARE_PATH}	/home/${USERNAME_INSTALL}/${SHARE_FOLDER}	cifs	uid=1000,guest,rw,users,nofail	0	0" >> /etc/fstab
 
 else
 	echo ""
-	echo "No shares will be mounted"
-
 fi
-
-echo ""
-echo "Moving to next module..."
