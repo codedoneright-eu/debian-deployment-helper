24b0f9a67e742e5b91eead9f7f6fe2474ffdc83b
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2021-09-04 10:24:06 +0200

    Update 07_alias.sh

--- data/07_alias.sh
+++ data/07_alias.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # This module adds some bash aliases
 
-	echo "alias cdr_update='sudo apt update && apt list --upgradable'" >> /home/${SCRIPT_USER}/.bash_aliases
-	echo "alias cdr_upgrade='sudo apt upgrade -y'" >> /home/${SCRIPT_USER}/.bash_aliases
-
-echo ""
-echo "Moving to next module..."
+echo "alias cdr_update='sudo apt update && apt list --upgradable'" >> /home/${USERNAME_INSTALL}/.bash_aliases
+echo "alias cdr_upgrade='sudo apt upgrade -y'" >> /home/${USERNAME_INSTALL}/.bash_aliases
