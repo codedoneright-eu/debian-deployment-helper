5297a7d570d45cd991f03dbc0caf6e1708e69636
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2021-09-04 10:22:31 +0200

    Update 02_packages_system.sh

--- data/02_packages_system.sh
+++ data/02_packages_system.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-#System
+#System packages
 apt install apt-xapian-index -y
 apt install bash-completion -y
 apt install cifs-utils -y
 apt install sudo -y
 apt install expect -y
-
-echo ""
-echo "System packages have been installed"
-echo "Moving to next module..."
