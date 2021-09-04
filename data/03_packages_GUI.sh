eafa68b4879efd27cf38a200e0c927e45fa99d30
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2021-09-04 10:22:49 +0200

    Update 03_packages_GUI.sh

--- data/03_packages_GUI.sh
+++ data/03_packages_GUI.sh
@@ -3,23 +3,19 @@
 #This will install GNOME desktop environment
 if [ $GUI_SELECT = g ]; then
 
-	echo ""
-	echo "Installing GNOME..."
 	apt install gnome -y
 	apt install gnome-shell-extensions -y
 	apt install gnome-tweak-tool -y
 	apt install gpaste -y
 	apt install gnome-shell-extensions-gpaste -y
-	apt install gnome-tweak -y
 	apt install gnome-tweaks -y
 
 #This will install XFCE4 desktop environment
 elif [ $GUI_SELECT = x ]; then
 
-	echo ""
-	echo "Installing XFCE4..."
 	apt install xfce4 -y
 	apt install xfce4-power-manager -y
+	apt install xfce4-screenshooter -y
 	apt install lightdm -y
 	apt install slick-greeter -y
 	apt install nm-tray -y
@@ -31,13 +27,6 @@ elif [ $GUI_SELECT = x ]; then
 	apt install xfce4-clipman -y
 	apt purge gnome-keyring -y
 
-#No packages, skip this module entirely
 else
-
 	echo ""
-	echo "No GUI will be installed"
-
 fi
-
-echo ""
-echo "Moving to next module..."
