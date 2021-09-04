eb74e81aafb6c833ce392cab1cf147e17fbe8cc0
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2021-09-04 10:19:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,49 +1,56 @@
 #!/bin/bash
+#Version: 1.4
+#Additional information available in read.me file
 
 clear
 
+echo "Debian Deployment Helper for GNU/Linux Debian 11 (Buster)"
 echo ""
-echo "Debian Deployment Helper for Debian GNU/Linux 11 Buster"
-echo ""
-echo "Before proceeding make sure that your PC is connected to your router via"
-echo "cable. This script assumes cable connection for the initial system setup."
+echo "Before proceeding, make sure that your PC is connected to your router via"
+echo "cable. DDH script assumes cable connection for the initial system setup."
 echo "You can connect to WiFi after the reboot."
 echo ""
+echo "Script amends sources.list for network mirrors and adds non-free and"
+echo "contrib repositories."
 echo "Changelog and additional information available in read.me file"
-echo ""
 
+echo ""
 read -p "Press enter to continue or Ctrl+C to abort " foo
 
-
 #Collect information from user
-echo ""
-echo "Please answer a few questions so that the script can tailor the experience"
-echo "for your needs. If you refuse to install desktop environment no utilities"
-echo "will be installed, as they rely on a GUI."
+clear
 
-#Check if user wants to install GUI, if no GUI is installed utilities will not
-#be installed
+echo "Please answer a few questions so that the script can tailor the experience"
+echo "to your needs. To install utilities you have to install GUI as well."
+echo "Yes/No prompts should be answered with lowercase y/n."
+
+#Check username for uid=1000
+USERNAME_INSTALL="$(cat /etc/passwd | grep 1000 | awk -F: '{print $1}')"
+
+#Ask if the user wants to grant sudo to uid=1000
+echo ""
+read -p "Would you like to add your non-administrative user to sudo group (y/n): " SUDO_GIVE
+
+	if [ $SUDO_GIVE = y ]; then
+		usermod -aG sudo ${USERNAME_INSTALL}
+		echo ""
+		echo "User ${USERNAME_INSTALL} has been added to the sudo group"
+	else
+		echo ""
+	fi
+
+#Ask the user which GUI should be installed
 echo ""
 echo "Please choose desktop environment..."
 echo "GNOME - g"
 echo "xfce4 - x"
-echo "Skip GUI installation - no"
 
 echo ""
-read -p "Which GUI woud you like to install (g/x/no)? " GUI_SELECT
+read -p "Which GUI woud you like to install (g/x)? " GUI_SELECT
 
-#Check if user wants to install commonly used script
+#Ask if the user wants to install additional script files
 echo ""
-read -p "Would you like to install commonly used script (y/n)? " SCRIPT_INSTALL
-
-	if [ $SCRIPT_INSTALL = y ]; then
-		echo ""
-		echo "Please provide information for the account on this system..."
-		read -p "Username: " SCRIPT_USER
-	else
-		echo ""
-		echo "Script will not be installed..."
-	fi
+read -p "Would you like to install additional script files (y/n)? " SCRIPT_INSTALL
 
 #Check if the user wants to add a network share
 echo ""
@@ -51,63 +58,66 @@ read -p "Would you like to mount a network share (y/n)? " SHARE_INSTALL
 
 	if [ $SHARE_INSTALL = y ]; then
 		echo ""
-		echo "Please provide information for the account on this system..."
-		read -p "Username: " SHARE_USER
-		read -p "Folder name for the share: " SHARE_FOLDER
-		read -p "Network share path: " SHARE_PATH
+		echo "Please provide..."
+		read -p "Folder name for the share to be created in ~/ : " SHARE_FOLDER
+		read -p "Network file share path: " SHARE_PATH
 	else
 		echo ""
-		echo "Network share will not be installed..."
 	fi
 
-#Grant sudo to a non-administrative user
+#Ask if the user wants to add VPN
 echo ""
-read -p "Would you like to add your non-administrative user to sudo group (y/n): " SUDO_GIVE
-
-	if [ $SUDO_GIVE = y ]; then
-		echo ""
-		read -p "Please specify which user: " SUDO_USER
-		usermod -aG sudo ${SUDO_USER}
-		echo "User ${SUDO_USER} has been added to the sudo group"
-	else
-		echo ""
-		echo "No user has been added to sudo group"
-	fi
+read -p "Would you like to install VPN service? (y/n): " VPN_INSTALL
 
 #Ask user if wants to reboot when finished
 echo ""
-read -p "Would you like to reboot when finished? (y/n): " REBOOT_NOW
+read -p "Would you like to shutdown the system when finished? (y/n): " SHUTDOWN_NOW
 
 #Export variables for use with other modules
+export USERNAME_INSTALL
+export SUDO_GIVE
 export GUI_SELECT
 export SCRIPT_INSTALL
-export SCRIPT_USER
 export SHARE_INSTALL
-export SHARE_USER
 export SHARE_FOLDER
 export SHARE_PATH
+export VPN_INSTALL
 
 #Display summary
-echo ""
-echo "Desktop to be installed:           ${GUI_SELECT}"
-echo "Commonly used script installation: ${SCRIPT_INSTALL}"
-echo "Script installation for user:      ${SCRIPT_USER}"
-echo "Install shares:                    ${SHARE_INSTALL}"
-echo "NFS share user:                    ${SHARE_USER}"
-echo "NFS share folder:                  ${SHARE_FOLDER}"
-echo "NFS share path:                    ${SHARE_PATH}"
-echo "Reboot after finishing:            ${REBOOT_NOW}"
-echo ""
-echo "Type 'alias' in terminal after the reboot to see new aliases"
-echo "You remove Debian Deployment Helper from your disk after the reboot."
+clear
 
+echo "Please check the summary below..."
+echo ""
+echo "Username:                          ${USERNAME_INSTALL}"
+	if [ $SUDO_GIVE = y ]; then
+		echo "                                   sudo granted"
+	else
+		echo "                                   sudo not granted"
+	fi
+echo "Desktop to be installed:           ${GUI_SELECT}"
+echo "Additional script installation:    ${SCRIPT_INSTALL}"
+echo "Network share added:               ${SHARE_INSTALL}"
+	if [ $SHARE_INSTALL = y ]; then
+		echo "NFS share folder to create:        ${SHARE_FOLDER}"
+		echo "NFS share path:                    ${SHARE_PATH}"
+	else
+		echo "                                   no changes to fstab"
+	fi
+echo "Install VPN service:               ${VPN_INSTALL}"
+echo "Shutdown after finishing:          ${SHUTDOWN_NOW}"
+echo ""
+echo "Type 'alias' in terminal after the reboot to see new aliases."
+echo "You can remove Debian Deployment Helper from your disk after rebooting"
+echo ""
+echo "Script will start working on your system after this prompt."
+echo "Time needed for completion depends on your system and internet speed"
 echo ""
 read -p "Press enter to continue or Ctrl+C to abort " foo
 
-#Script starts working here
+#Script starts actual work here
+clear
 
-#Amend sources.list
-#Sources contain contrib and non-free mirrors
+#Amend sources.list, sources contain contrib and non-free mirrors
 echo "
 deb http://deb.debian.org/debian bullseye main contrib non-free
 deb-src http://deb.debian.org/debian bullseye main contrib non-free
@@ -118,38 +128,38 @@ deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib no
 deb http://deb.debian.org/debian bullseye-updates main contrib non-free
 deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free" > /etc/apt/sources.list
 
-echo ""
-echo "/etc/apt/sources.list file has been amended to contain contrib and non-free mirrors"
-
 #Connect to the internet by establishing connection
-echo ""
 echo "Establishing connection and updating the system..."
 dhclient
 apt update
 apt upgrade -y
 
-#Install system and additional commonly used packages
+#Install GUI + utilities
 ./data/02_packages_system.sh
 ./data/03_packages_GUI.sh
 ./data/04_packages_utilities.sh
 
-#Install useful script and create aliases for them
+#Install additional script files and create aliases for them
 ./data/05_add_script.sh
 
-#Amend fstab for network shares
+#Amend fstab for network share
 ./data/06_shares.sh
 
 #Add aliases
 ./data/07_alias.sh
 
-#Reboot section
-	if [ $REBOOT_NOW = y ]; then
-		reboot
+#Add vpn
+./data/08_vpn.sh
+
+#Create log
+./data/98_log.sh
+
+#Shutdown section
+	if [ $SHUTDOWN_NOW = y ]; then
+		shutdown now
 	else
 		echo ""
-		echo "Please reboot your system"
+		echo "Script has finished, please reboot your system..."
 	fi
 
-#All done
 echo ""
-echo "Enjoy your new system!"
