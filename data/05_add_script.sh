9df7ed284eeb24afcbcbb3062c56e54b5a4b5b65
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2021-09-04 10:23:25 +0200

    Update 05_add_script.sh

--- data/05_add_script.sh
+++ data/05_add_script.sh
@@ -2,24 +2,18 @@
 
 if [ $SCRIPT_INSTALL = y ]; then
 
-	mkdir /var/zzz_script || echo "Folder already exists, please investigate..."
+	mkdir /var/zzz_script
 	cp ./data/script/*.sh /var/zzz_script/
-	chown ${SCRIPT_USER}:${SCRIPT_USER} -R /var/zzz_script/
+	chown ${USERNAME_INSTALL}:${USERNAME_INSTALL} -R /var/zzz_script/
 	chmod +x -R /var/zzz_script/
-	echo "alias cdr_backup='/var/zzz_script/cdr_backup.sh'" >> /home/${SCRIPT_USER}/.bash_aliases
-	echo "alias cdr_pwd='/var/zzz_script/cdr_pwd.sh'" >> /home/${SCRIPT_USER}/.bash_aliases
-	echo "alias cdr_ssh='/var/zzz_script/cdr_ssh.sh'" >> /home/${SCRIPT_USER}/.bash_aliases
-	echo "alias cdr_sync='/var/zzz_script/cdr_sync.sh'" >> /home/${SCRIPT_USER}/.bash_aliases
 
-	echo ""
-	echo "Commonly used script has been installed in /var/zzz_script/"
+	echo "alias cdr_backup='/var/zzz_script/cdr_backup.sh'" >> /home/${USERNAME_INSTALL}/.bash_aliases
+	echo "alias cdr_pwd='/var/zzz_script/cdr_pwd.sh'" >> /home/${USERNAME_INSTALL}/.bash_aliases
+	echo "alias cdr_ssh='/var/zzz_script/cdr_ssh.sh'" >> /home/${USERNAME_INSTALL}/.bash_aliases
+	echo "alias cdr_sync='/var/zzz_script/cdr_sync.sh'" >> /home/${USERNAME_INSTALL}/.bash_aliases
+
+	chown ${USERNAME_INSTALL}:${USERNAME_INSTALL} /home/${USERNAME_INSTALL}/.bash_aliases
 
 else
-
 	echo ""
-	echo "No script will be installed"
-
 fi
-
-echo ""
-echo "Moving to next module..."
