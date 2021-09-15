9a297ed52f5b58b02e1cb6acef8cb81c7c768d09
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2021-09-15 17:13:08 +0200

    Update 07_alias.sh

--- data/07_alias.sh
+++ data/07_alias.sh
@@ -3,3 +3,6 @@
 
 echo "alias cdr_update='sudo apt update && apt list --upgradable'" >> /home/${USERNAME_INSTALL}/.bash_aliases
 echo "alias cdr_upgrade='sudo apt upgrade -y'" >> /home/${USERNAME_INSTALL}/.bash_aliases
+echo "alias shh='sudo shutdown 90'" >> /home/${USERNAME_INSTALL}/.bash_aliases
+
+echo "Package update and upgrade avaliable via 'cdr_update' and 'cdr_upgrade' aliases" >> ${DDH_LOG}
