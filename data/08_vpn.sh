b81af31f66f6e1944d1b435d45e0590609d3308b
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2021-09-15 18:38:34 +0200

    Update 08_vpn.sh

--- data/08_vpn.sh
+++ data/08_vpn.sh
@@ -7,7 +7,7 @@ if [ $VPN_INSTALL = y ]; then
 	cp ./data/vpn/* /home/${USERNAME_INSTALL}/.vpn/
 	chown ${USERNAME_INSTALL}:${USERNAME_INSTALL} -R /home/${USERNAME_INSTALL}/.vpn/
 	chmod +x -R /home/${USERNAME_INSTALL}/.vpn/
-	echo "alias cdr_vpn='sudo openvpn --config /home/${USERNAME_INSTALL}/.vpn/${VPN_CONF} --daemon'" >> /home/${USERNAME_INSTALL}/.bash_aliases
+	echo "alias cdr_vpn='cd ~/.vpn/ && sudo openvpn --config ${VPN_CONF} --daemon'" >> /home/${USERNAME_INSTALL}/.bash_aliases
 
 	echo "VPN connection is available via 'cdr_vpn' alias" >> ${DDH_LOG}
 else
