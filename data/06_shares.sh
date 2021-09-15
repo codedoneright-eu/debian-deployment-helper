aa38c3fdaec9b91b69d9347c12484e17b87c9266
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2021-09-15 17:12:50 +0200

    Update 06_shares.sh

--- data/06_shares.sh
+++ data/06_shares.sh
@@ -7,6 +7,7 @@ if [ $SHARE_INSTALL = y ]; then
 
 	echo "${SHARE_PATH}	/home/${USERNAME_INSTALL}/${SHARE_FOLDER}	cifs	uid=1000,guest,rw,users,nofail	0	0" >> /etc/fstab
 
+	echo "Network share has been mounted in ~/${SHARE_FOLDER}" >> ${DDH_LOG}
 else
 	echo ""
 fi
