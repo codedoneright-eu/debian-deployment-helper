d7e896db3c7fd59b43dc5b0634254ed8793abb09
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2021-09-15 17:17:17 +0200

    Update 13_wallpaper.sh

--- data/13_wallpaper.sh
+++ data/13_wallpaper.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cp ./data/wallpaper/* /home/${USERNAME_INSTALL}/.vpn/
+#NOT FUNCTIONAL YET, just copies to user's ~/ for now
+
+cp ./data/wallpaper/* /home/${USERNAME_INSTALL}/
 echo "If you put wallpapers in the folder they have been copied" >> ${DDH_LOG}
