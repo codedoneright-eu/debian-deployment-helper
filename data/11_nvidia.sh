2e33eba4878c044d3e6e5d68f1c5f95ee3ad4406
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2021-09-15 17:26:46 +0200

    Update 11_nvidia.sh

--- data/11_nvidia.sh
+++ data/11_nvidia.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ $NVIDIA_DRIVERS = y ]; then
-	apt install nvidia-driver 
-	apt install firmware-misc-nonfree
+	apt install nvidia-driver -y
+	apt install firmware-misc-nonfree -y
 	echo "nVidia drivers installed" >> ${DDH_LOG}
 else
 	echo ""
