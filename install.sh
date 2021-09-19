da8a3ea3cd2b87400fcba111b0b94ef6313a2120
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2021-09-19 13:26:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,225 +1,115 @@
 #!/bin/bash
-#Version: 1.5
-#Additional information available in read.me file
 
-clear
+DATE="$(which date)"
+TIMESTAMP=`$DATE '+%Y.%m.%d'`
 
-echo "Debian Deployment Helper for GNU/Linux Debian 11 (Buster)"
-echo ""
-echo "Before proceeding, make sure that your PC is connected to your router via"
-echo "cable. DDH script assumes cable connection for the initial system setup."
-echo "You can connect to WiFi after the reboot."
-echo ""
-echo "Script amends sources.list for network mirrors and adds non-free and"
-echo "contrib repositories."
-echo "Changelog and additional information available in read.me file"
+export WORK_DIR=`pwd`
 
 echo ""
-read -p "Press enter to continue or Ctrl+C to abort " foo
-
-#Collect information from user
-clear
-
-echo "Please answer a few questions so that the script can tailor the experience"
-echo "to your needs. To install utilities you have to install GUI as well."
-echo ""
-echo "Yes/No prompts should be answered with a lowercase y/n."
-
-#Check username for uid=1000
-USERNAME_INSTALL="$(cat /etc/passwd | grep 1000 | awk -F: '{print $1}')"
-
-#Ask if the user wants to grant sudo to uid=1000
-echo ""
-read -p "Would you like to add your non-administrative user to sudo group (y/n): " SUDO_GIVE
-
-	if [ $SUDO_GIVE = y ]; then
-		usermod -aG sudo ${USERNAME_INSTALL}
-		echo "User ${USERNAME_INSTALL} has been added to the sudo group"
-	else
-		echo ""
-	fi
-
-#Ask the user which GUI should be installed
-echo ""
-echo "Which desktop environment do you want to install..."
-echo "GNOME - g"
-echo "xfce4 - x"
+echo "Linux Backup and Restore Helper"
 
 echo ""
-read -p "Which GUI woud you like to install (g/x)? " GUI_SELECT
-
-#Ask if the user wants to install additional script files
+echo "Hello $USER, Please follow on-screen instructions"
 echo ""
-read -p "Would you like to install additional script files (y/n)? " SCRIPT_INSTALL
-
-#Check if the user wants to add a network share
+echo "This script creates and restores backups of a drive, sudo is required"
+echo "To find out drive letters run 'sudo fdisk -l' and identify your drive"
+echo "Script works from current working directory."
 echo ""
-read -p "Would you like to mount a network share (y/n)? " SHARE_INSTALL
-
-	if [ $SHARE_INSTALL = y ]; then
-		echo ""
-		echo "Please provide..."
-		read -p "Folder name for the share to be created in ~/ : " SHARE_FOLDER
-		read -p "Network file share path: " SHARE_PATH
-	else
-		echo ""
-	fi
-
-#Ask if the user wants to add VPN
+echo "Please select function..."
+echo "b: Make a backup"
+echo "r: Restore a backup"
+echo "q: quit"
 echo ""
-read -p "Would you like to install VPN service? (y/n): " VPN_INSTALL
 
-#Ask if the user wants to add spotify repository
-echo ""
-read -p "Would you like to add Spotify repository? (y/n): " SPOTIFY
+read -p "What would you like to do (b/r/q): " PART
 
-#Ask if the user wants to add Signal repository
-echo ""
-read -p "Would you like to add Signal repository? (y/n): " SIGNAL
+if [ $PART = b ]; then
 
-#Ask if the user wants to add other apps
-echo ""
-read -p "Would you like to download additional .deb files? (y/n): " OTHER_APPS
+    echo ""
+    echo "Which device would you like to clone?"
+    
+    read -p "Please provide the letter (sdX): " DRIVE_B
+    
+    echo ""
+    echo "Cloning /dev/sd${DRIVE_B}"
+    echo ""
+    echo "Output file name structure is NAME_${TIMESTAMP}.zip"
+    
+    read -p "Please specify NAME part: " NAMEB
+    
+    echo ""
+    echo "Output file will be named ${NAMEB}_${TIMESTAMP}.zip"
+    echo ""
+    echo "This will take a while. Please wait... "
+    echo ""
 
-#Ask if the user wants to add nVidia drivers PPA
-echo ""
-read -p "Would you like to install nVidia drivers? (y/n): " NVIDIA_DRIVERS
+    sudo dd if=/dev/sd${DRIVE_B} of=${NAMEB}_${TIMESTAMP}.img conv=noerror status=progress
 
-#Ask user if wants to reboot when finished
-echo ""
-read -p "Would you like to shutdown the system when finished? (y/n): " SHUTDOWN_NOW
+    echo ""
+    echo "Compressing ${NAMEB}_${TIMESTAMP}.img, please wait..."
+    
+    zip ${NAMEB}_$TIMESTAMP.zip ${NAMEB}_${TIMESTAMP}.img
 
-#Display summary
-clear
+    echo ""
+    echo "Removing ${NAMEB}_${TIMESTAMP}.img"
+    
+    sudo rm ${NAMEB}_${TIMESTAMP}.img
 
-echo "Please check the summary below..."
-echo ""
-echo "Username:                          ${USERNAME_INSTALL}"
-	if [ $SUDO_GIVE = y ]; then
-		echo "                                   sudo granted"
-	else
-		echo "                                   sudo NOT granted"
-	fi
-echo "Desktop to be installed:           ${GUI_SELECT}"
-echo "Additional script installation:    ${SCRIPT_INSTALL}"
-echo "Network share added:               ${SHARE_INSTALL}"
-	if [ $SHARE_INSTALL = y ]; then
-		echo "NFS share folder to create:        ${SHARE_FOLDER}"
-		echo "NFS share path:                    ${SHARE_PATH}"
-	else
-		echo "                                   no changes to fstab"
-	fi
-echo "Install VPN service:               ${VPN_INSTALL}"
-echo "Install Spotify repository:        ${SPOTIFY}"
-echo "Install Signal repository:         ${SIGNAL}"
-echo "Download other apps:               ${OTHER_APPS}"
-echo "Install nVidia drivers:            ${NVIDIA_DRIVERS}"
-echo "Shutdown after finishing:          ${SHUTDOWN_NOW}"
-echo ""
-echo "Script will start working on your system after this prompt."
-echo "Time needed for completion depends on your system and internet speed"
-echo ""
-echo "Log file will be created in /home/${USERNAME_INSTALL}/ddh-log.txt"
-echo ""
-read -p "Press enter to continue or Ctrl+C to abort " foo
+    SIZE="$(du -h ${NAMEB}_${TIMESTAMP}.zip | awk '{ print $1 }')"
 
-#Script starts actual work here
-clear
+    echo ""
+    echo "The file ${NAMEB}_${TIMESTAMP}.zip is about ${SIZE}."
+    echo "Full file path is ${WORK_DIR}/${NAMEB}_${TIMESTAMP}.zip"
 
-#Create log
-touch /home/${USERNAME_INSTALL}/ddh-log.txt
-chown ${USERNAME_INSTALL}:${USERNAME_INSTALL} /home/${USERNAME_INSTALL}/ddh-log.txt
+elif [ $PART = r ]; then
 
-DDH_LOG=/home/${USERNAME_INSTALL}/ddh-log.txt
+    echo ""
+    echo "This part will restore a backup."
+    echo ""
+    echo "Script looks for input file with the following pattern NAME_YYYY.MM.DD.zip"
+    echo ""
+    echo "To which device would you like to write?"
+    
+    read -p "Please provide the letter (sdX): " DRIVE_R
+    
+    echo ""
+    echo "Which file you want to restore?"
+    
+    read -p "Please specify NAME part of the name: " NAMER
+    read -p "Please specify YYYY part of the name: " YEAR
+    read -p "Please specify MM   part of the name: " MONTH
+    read -p "Please specify DD   part of the name: " DAY
+    
+    echo "Script will restore backup file named ${NAMER}_${YEAR}.${MONTH}.${DAY}.zip"
+    echo "To drive /dev/sd${DRIVE_R}"
+    echo ""
+    read -p "Press enter to continue or Ctrl+C to abort " foo
+    
+    echo ""
+    echo "Unzipping ${NAMER}_${YEAR}.${MONTH}.${DAY}.zip" 
+    echo "This will take a while. Please wait..."
+    echo ""
+    
+    unzip ${NAMER}_${YEAR}.${MONTH}.${DAY}.zip
 
-echo "Debian Deployment Helper log
-============================
-Quick system update and upgrade available via 'cdr_update' and 
-'cdr_upgrade' commands in CLI
-" >> ${DDH_LOG}
+    echo ""
+    echo "Writing ${NAMER}_${YEAR}.${MONTH}.${DAY}.img to /dev/sd${DRIVE_R}"
+    echo "This will take a while. Please wait..."
+    
+    sudo dd if=${NAMER}_${YEAR}.${MONTH}.${DAY}.img of=/dev/sd${DRIVE_R} conv=noerror status=progress
 
-#Add note in log if user granted sudo
-if [ $SUDO_GIVE = y ]; then
-	echo "User ${USERNAME_INSTALL} has been granted sudo privilege" >> ${DDH_LOG}
+    echo ""
+    echo "Removing ${NAMER}_${YEAR}.${MONTH}.${DAY}.img"
+    
+    sudo rm ${NAMER}_${YEAR}.${MONTH}.${DAY}.img
+
+    echo ""
+    echo "Drive /dev/sd${DRIVE_R} is ready for use"
+
 else
-	echo "User ${USERNAME_INSTALL} has not been granted sudo privilege" >> ${DDH_LOG}
+
+    echo "Nothing has been done..."
+
 fi
-
-#Export variables for use with other modules
-export USERNAME_INSTALL
-export SUDO_GIVE
-export GUI_SELECT
-export SCRIPT_INSTALL
-export SHARE_INSTALL
-export SHARE_FOLDER
-export SHARE_PATH
-export VPN_INSTALL
-export SPOTIFY
-export SIGNAL
-export OTHER_APPS
-export NVIDIA_DRIVERS
-export DDH_LOG
-
-#Amend sources.list, sources contain contrib and non-free mirrors
-echo "
-deb http://deb.debian.org/debian bullseye main contrib non-free
-deb-src http://deb.debian.org/debian bullseye main contrib non-free
-
-deb http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
-deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
-
-deb http://deb.debian.org/debian bullseye-updates main contrib non-free
-deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free" > /etc/apt/sources.list
-
-#Connect to the internet by establishing connection
-echo "Establishing connection and updating the system..."
-dhclient
-apt update
-apt upgrade -y
-
-#Install GUI + utilities
-./data/02_packages_system.sh
-./data/03_packages_GUI.sh
-./data/04_packages_utilities.sh
-
-#Install additional script files and create aliases for them
-./data/05_add_script.sh
-
-#Amend fstab for network share
-./data/06_shares.sh
-
-#Add aliases
-./data/07_alias.sh
-
-#Add vpn
-./data/08_vpn.sh
-
-#Add spotify repository
-./data/09_spotify.sh
-
-#Install other non-free apps
-./data/10_other_apps.sh
-
-#Install nVidia drivers
-./data/11_nvidia.sh
-
-#Add spotify repository
-./data/12_signal.sh
-
-#Put wallpaper in the correct place for immediate use
-#./data/13_wallpaper.sh
-
-#Write out all installed packages (moved here so the long list is at the end)
-echo "
-Following packages have been installed:" >> ${DDH_LOG}
-awk '$3~/^install$/ {print $4;}' /var/log/dpkg.log >> ${DDH_LOG}
-
-#Shutdown section
-	if [ $SHUTDOWN_NOW = y ]; then
-		shutdown now
-	else
-		echo ""
-		echo "Script has finished, please reboot your system..."
-	fi
 
 echo ""
