cfe6b3ab6729fa53d74214deb75f118665eb8d17
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2021-09-20 00:33:44 +0200

    Delete 13_wallpaper.sh

--- data/13_wallpaper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#NOT FUNCTIONAL YET, just copies to user's ~/ for now
-
-cp ./data/wallpaper/* /home/${USERNAME_INSTALL}/
-echo "If you put wallpapers in the folder they have been copied" >> ${DDH_LOG}
