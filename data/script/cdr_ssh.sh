4bfbcb30ae085bce7c284ccef892438e2b85f85c
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2021-09-20 17:34:51 +0200

    Update cdr_ssh.sh

--- data/script/cdr_ssh.sh
+++ data/script/cdr_ssh.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/expect -f
 
-#Provide login data by changing foo to relevant string
-USER=foo
-IP=foo
-PASSWORD=foo
+#Provide login data by replacing relevant string
+set CDR_USERNAME "put_username_here"
+set CDR_IP "put_server_IP_here"
+set CDR_PW "put_password_here"
 
-spawn ssh ${USER}@${IP}
+spawn ssh $CDR_USERNAME@$CDR_IP
 expect "assword:"
-send "${PASSWORD}\r"
+send "$CDR_PW\r"
 interact
