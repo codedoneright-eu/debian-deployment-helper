a1c7e4942ebae719e946dd13f0a60fcd1c9f6136
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2024-09-26 16:25:35 +0200

    Utilities fix
    If wrong GUI was selected by default installs GNOME - added installation of utilities in such a case as well

--- data/04_packages_utilities.sh
+++ data/04_packages_utilities.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $GUI_SELECT = g ]; then
-	
+
 	apt install evince -y 
 	apt install gparted -y 
 	apt install htop -y 
@@ -49,6 +49,28 @@ elif [ $GUI_SELECT = x ]; then
 	apt install clamtk -y
 
 else
-	echo ""
+
+	apt install evince -y
+	apt install gparted -y
+	apt install htop -y
+	apt install kate -y
+	apt install vlc -y
+	apt install zip -y
+	apt install spyder -y
+
+	apt install gimp -y
+	apt install mcomix -y
+	apt install shotcut -y
+	apt install obs-studio -y
+	apt install dolphin-emu -y
+
+	apt install firefox-esr -y
+	apt install filezilla -y
+	apt install thunderbird -y
+
+	apt install clamav -y
+	apt install clamtk -y
+
+	echo "Utilities have been installed" >> ${DDH_LOG}
 fi
 
